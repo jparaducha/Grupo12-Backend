11b2357fa5a67ded1074b64adfaa1bdead5f8b5c
Salvador <delacolinasalvador@gmail.com>
2022-04-20 15:46:18 -0300

    refactored shopping cart post route to take an array instead of a str

--- routes/shoppingCart.js
+++ routes/shoppingCart.js
@@ -1,5 +1,8 @@
 const router = require("express").Router();
+const { user } = require("pg/lib/defaults");
 const { Shopping_cart , User, Product, Stock, sequelize} = require("../db");
+
+//--------------------------------------------------------------------------------------------
 
 router.get("/", async (req,res)=>{
     try {
@@ -17,79 +20,113 @@ router.get("/", async (req,res)=>{
     }
 })
 
+//--------------------------------------------------------------------------------------------
 
 router.post("/", async (req,res)=> {
 
-    try {
-        
-    const { buyer_id , product_id , seller_id , quantity } = req.body;
+    const { buyer_id , products } = req.body;
 
-    if(!product_id) return res.json("Falta el product id");
-    if(!buyer_id) return res.json("Falta el buyer id");
-    if(!seller_id) return res.json("Falta el seller id");
-    if(!quantity) return res.json("Falta la cantidad de unidades");
-
-    const stock  = await Stock.findOne({
-        where : {
-            "user_id" : seller_id,
-            "product_id" : product_id,
-            },
-    });
-
-    if(!stock) return res.send('Actualmente no hay stock de ese producto')
-
-    if (stock.quantity<quantity) return res.send('Actualmente hay menos stock a ese precio del solicitado')
-
-    const product = await Product.findOne({
-        where : {
-            "product_id" : product_id
-        }
-    })
-
-    if (!product) return res.status(400).send('El producto no se encuentra listado')
-
-    const shopping_cart = await Shopping_cart.findOne({
-        where : { 
-            'buyer_id' : buyer_id ,
-            'seller_id' : seller_id ,
-            'product_id' : product_id,
-            'unit_price' : stock.unit_price,
-            //'approved' : true    
-        }
-    })
-
-    if(shopping_cart) {
-        shopping_cart.quantity = shopping_cart.quantity + quantity
-        shopping_cart.save()
-        return res.status(200).send(shopping_cart)
-    }
+    if(!products) return res.json("Error : Missing products in request");
+    if(!buyer_id) return res.json("Error : Missing buyer_id in request");
+    if(!(typeof products !== Array)) return res.json("Error : products is not an array");
+    
+    const productsToAdd = [];
+    const result = []
 
     const buyer = await User.findOne({ where : { "user_id" : buyer_id }});
 
-    if (!buyer) return res.status(400).send('El usuario comprador no existe')
-    
-    const cart = await Shopping_cart.create(
-        { 
-        "product":  product,
-        "product_id": product_id,
-        "unit_price" : stock.unit_price,
-        "buyer_id": buyer_id,
-        "quantity" : quantity,
-        "seller_id" : seller_id
+    for (let productToAdd of products){
+        console.log('fetching product')
+        
+        var product = await Product.findOne({
+            where : {
+                'product_id' : productToAdd.product_id
+            }
         });
-    
-    buyer.addShopping_cart(cart).then((cart)=>{
-        stock.quantity = stock.quantity - quantity;
-        stock.save();
-        product.stock = product.stock - quantity;
-        product.save();
-        res.status(200).send(cart);
-    });
 
-    } catch (error) {
-        console.log(error.message);
-    }
+        console.log('product fetched')
+
+        //------------------------------
+
+        console.log('fetching stock')
+
+        const stock = await Stock.findOne({
+            where : {
+                "user_id" : productToAdd.seller_id,
+                "product_id" : productToAdd.product_id,
+                },
+        })
+
+        console.log('stocks fetched')
+
+        //------------------------------
+
+        console.log('adjusting stock')
+        console.log(stock.quantity)
+        stock.quantity = stock.quantity - productToAdd.quantity;
+        stock.save();
+        product.stock = product.stock - productToAdd.quantity;
+        product.save();
+        
+        //------------------------------
+
+        console.log('checking for existent product')
+
+        const overwrite = await Shopping_cart.findOne({
+            where : {
+                'buyer_id' : buyer_id,
+                'product_id' : productToAdd.product_id,
+                'seller_id' : productToAdd.seller_id
+            }
+        })
+
+        if (overwrite) {
+            console.log('overwriting')
+            overwrite.quantity = productToAdd.quantity;
+            overwrite.product = product;
+            overwrite.save();
+
+            //------------------------------
+
+            console.log('adjusting stock')
+            console.log(stock.quantity)
+            stock.quantity = stock.quantity + productToAdd.quantity;
+            stock.save();
+            product.stock = product.stock + productToAdd.quantity;
+            product.save();
+
+            //------------------------------
+
+            result.push(overwrite)
+        } else{
+            console.log('creating shopping cart promise')
+            const cart = Shopping_cart.create(
+                { 
+                "product":  product,
+                "product_id": productToAdd.product_id,
+                "unit_price" : stock.unit_price,
+                "buyer_id": buyer_id,
+                "quantity" : productToAdd.quantity,
+                "seller_id" : productToAdd.seller_id
+                })
+                productsToAdd.push(cart)
+            }
+        };
+
+    Promise.all(productsToAdd).then((productsAdded) => {
+        console.log('promises resolved')
+        productsAdded.forEach((product)=>{
+            console.log('adding carts to users')
+            buyer.addShopping_cart(product);
+            result.push(product);
+        })
+    }).then(()=>{
+        console.log('res')
+        return res.status(200).send(result)
+    })
 })
+
+//--------------------------------------------------------------------------------------------
 
 router.delete('/' , async (req,res) =>{
     
@@ -130,6 +167,8 @@ router.delete('/' , async (req,res) =>{
     })
 })
 
+//--------------------------------------------------------------------------------------------
+
 router.patch('/' , async (req,res) => {
 
     const {buyer_id , seller_id , product_id , newQuantity} = req.body;
@@ -159,6 +198,8 @@ router.patch('/' , async (req,res) => {
     })
 
 })
+
+//--------------------------------------------------------------------------------------------
 
 
 module.exports = router;
