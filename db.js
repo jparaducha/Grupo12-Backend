67cc36a90a839313c99dcc9397afaac9b915a045
Juan Carlos Goicochea <juancgoicochea@gmail.com>
2022-04-21 21:45:25 -0500

    Updated files for deploy

--- db.js
+++ db.js
@@ -1,6 +1,10 @@
 const Sequelize = require("sequelize");
 const fs = require('fs');
 const path = require('path');
+const { Server } = require("http");
+const express = require('express');
+
+const server = express();
 
 require("dotenv").config();
 
@@ -9,18 +13,39 @@ const {
     DB_PORT,
     DB_USER,
     DB_PASSWORD,
-    DATABASE_URL
+    DATABASE_URL,
+    DB_NAME
 } = process.env
-
-const sequelize = new Sequelize(DATABASE_URL, DB_USER, DB_PASSWORD, {
+let sequelize =
+  process.env.NODE_ENV === "production"
+    ? new Sequelize({
+        database: DB_NAME,
+        dialect: "postgres",
+        host: DB_HOST,
+        port: 5432,
+        username: DB_USER,
+        password: DB_PASSWORD,
+        pool: {
+          max: 3,
+          min: 1,
+          idle: 10000,
+        },
+        dialectOptions: {
+          ssl: {
+            require: true,
+            // Ref.: https://github.com/brianc/node-postgres/issues/2009
+            rejectUnauthorized: false,
+          },
+          keepAlive: true,
+        },
+        ssl: true,
+      })
+    : new Sequelize(DATABASE_URL, DB_USER, DB_PASSWORD, {
     host : DB_HOST,
     port : DB_PORT,
     dialect : "postgres",
     logging : false
 })
-
-
-
 
 const basename = path.basename(__filename);
 
@@ -51,8 +76,10 @@ Product.belongsToMany(User , { through: Stock , foreignKey:'product_id'});
 Category.hasMany(Category, { as: 'children', foreignKey:'parent_id'})
 // Category.hasMany(Product,  {foreignKey : 'category_id'})
 
-sequelize.sync( {alter: true} ).then((data)=>{
+sequelize.sync( {force: false} ).then((data)=>{
+  server.listen(process.env.PORT, () => {
     console.log("DB synced");
+  })
 })
 .catch((err)=>{
     console.log(err);
--- index.js
+++ index.js
@@ -7,6 +7,13 @@ const { APIPORT } = process.env;
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+  });
 
 
 app.use("/auth", require("./routes/auth"));
@@ -25,3 +32,5 @@ app.use("/admin", require("./routes/admin"));
 
 
 app.listen( APIPORT , ()=> console.log(`Server running on port ${APIPORT}`));
+
+module.exports = app;
