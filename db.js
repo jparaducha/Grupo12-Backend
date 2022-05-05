216bf9b4b0ac76fb4800ca04c4c63706f4e97b16
Juan Paraducha <68435382+jparaducha@users.noreply.github.com>
2022-05-04 23:19:29 -0300

    force:true

--- db.js
+++ db.js
@@ -93,7 +93,7 @@ Product.belongsToMany(User, { through: Movement });
 // Category.hasMany(Product,  {foreignKey : 'category_id'})
 
 sequelize
-  .sync({ alter: true })
+  .sync({ force: true })
   .then((data) => {
     console.log("DB synced");
   })
